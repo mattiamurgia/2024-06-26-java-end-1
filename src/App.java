99c6ce7cf63042390428bfcb3a164030c2d58137
mattiamurgia <mattiamurgiacontatto@gmail.com>
2024-06-26 12:15:38 +0200

    ES1: Sistema di Prenotazioni

--- src/App.java
+++ src/App.java
@@ -1,5 +1,36 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        es1();
+    }
+
+    public static void es1() {
+
+        Bookings b = new Bookings(5);
+
+        // Stampa tutti i posti liberi
+        System.out.println(b);
+
+        // Prenota i posti
+        b.bookPlace(0);
+        b.bookPlace(1);
+        b.bookPlace(2);
+        b.bookPlace(3);
+        b.bookPlace(4);
+        System.out.println(b);
+
+        // Mostra che il posto è occupato
+        b.bookPlace(0);
+        b.cancelBook(0);
+        b.cancelBook(4);
+        System.out.println(b);
+
+        // Prenotazione di un posto maggiore getPlaces().length
+        b.bookPlace(6);
+
+        // Verifica se ci sono posti liberi
+        System.out.println();
+        System.out.println("-----Free-----");
+        b.findFreePlaces();
     }
 }
---
+++ src/Bookings.java
@@ -0,0 +1,65 @@
+
+import java.util.Arrays;
+
+public class Bookings {
+
+    private boolean[] places;
+
+    public Bookings(int nPlaces) {
+        setPlaces(new boolean[nPlaces]);
+
+    }
+
+    public boolean[] getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(boolean[] places) {
+        this.places = places;
+    }
+
+    // Add Reservation Method
+    public void bookPlace(int iPlace) {
+        if (iPlace < 0 || iPlace >= getPlaces().length) {
+            System.out.println("iPlace non valido");
+        } else if (getPlaces()[iPlace]) {
+            System.out.println("Il posto " + (iPlace + 1) + " è già occupato");
+        } else {
+            getPlaces()[iPlace] = true;
+            System.out.println("Prenotato il posto " + (iPlace + 1));
+        }
+    }
+
+    // Booking Cancellation Method
+    public void cancelBook(int iPlace) {
+        if (iPlace < 0 || iPlace >= getPlaces().length) {
+            System.out.println("iPlace non valido");
+        } else if (!getPlaces()[iPlace]) {
+            System.out.println("Il posto " + (iPlace + 1) + " non è occupato");
+        } else {
+            getPlaces()[iPlace] = false;
+            System.out.println("Annullata la prenotazione del posto " + (iPlace + 1));
+        }
+    }
+
+    // Method Checking Vacancies
+    public void findFreePlaces() {
+        boolean freePlace = false;
+        System.out.print("I posti liberi sono: ");
+        for (int i = 0; i < getPlaces().length; i++) {
+            if (!getPlaces()[i]) {
+                System.out.print((i + 1) + " ");
+                freePlace = true;
+            }
+        }
+        if (!freePlace) {
+            System.out.println("Non ci sono posti liberi");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Bookings: " + Arrays.toString(getPlaces()) + "\n";
+    }
+
+}
